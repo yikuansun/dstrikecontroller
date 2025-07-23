7322936ef3bd2c17f3dfe9a56dbe74ed0935c011
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-23 14:58:27 -0400

    improve client
    cool

--- src/routes/clientTest/+page.svelte
+++ src/routes/clientTest/+page.svelte
@@ -8,17 +8,28 @@
     let socket;
     let logs = [];
 
+    /** @type {URL} */
+    let controllerURL;
+
     onMount(() => {
         socket = skio.get();
         socket.on(`output`, data => {
             logs = [...logs, data];
         });
         console.log(socket)
+        controllerURL = new URL(`/${socket.id}`, location.origin);
     })
 </script>
 
 {#if socket}
     <pre>Socket ID: {socket.id}</pre>
+{/if}
+
+{#if controllerURL}
+    <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={controllerURL.toString()}"
+        alt={controllerURL.toString()} />
+    <br />
+    <a href={controllerURL.toString()} target="_blank">{controllerURL.toString()}</a>
 {/if}
 <pre>Logs:</pre>
 {#each logs as log}
