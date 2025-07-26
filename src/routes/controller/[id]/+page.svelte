7739a12f04385f4799ea9318631e27f5b5a0cf92
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-26 16:15:13 -0400

    Add d-pad
    Looks kinda dumb atm but ok

--- src/routes/controller/[id]/+page.svelte
+++ src/routes/controller/[id]/+page.svelte
@@ -18,6 +18,13 @@
         distance: 0,
     };
 
+    let dPad = {
+        up: false,
+        down: false,
+        left: false,
+        right: false,
+    };
+
     /**
      * Trigger input on websocket
      * @param {string} button
@@ -109,6 +116,106 @@
             }} />
         <circle cx={width - 200 + stickRight.distance * Math.cos(stickRight.angle) * 100} cy={height - 150 + stickRight.distance * Math.sin(stickRight.angle) * 100} r="80" fill="grey"
             style:pointer-events="none" />
+    </g>
+    <g>
+        <circle cx="150" cy="150" r="100" fill="transparent"
+            on:touchstart={(e) => {
+                e.preventDefault();
+                let r = e.currentTarget.getBoundingClientRect().width / 2;
+                let cx = e.currentTarget.getBoundingClientRect().x + r;
+                let cy = e.currentTarget.getBoundingClientRect().y + r;
+                let touch = e.changedTouches[0];
+                let before = Object.assign({}, dPad);
+                if (touch.clientY < cy - r * 0.4) {
+                    dPad.up = true;
+                    dPad.down = false;
+                }
+                else if (touch.clientY > cy + r * 0.4) {
+                    dPad.down = true;
+                    dPad.up = false;
+                }
+                else {
+                    dPad.down = false;
+                    dPad.up = false;
+                }
+                if (touch.clientX < cx - r * 0.4) {
+                    dPad.left = true;
+                    dPad.right = false;
+                }
+                else if (touch.clientX > cx + r * 0.4) {
+                    dPad.right = true;
+                    dPad.left = false;
+                }
+                else {
+                    dPad.right = false;
+                    dPad.left = false;
+                }
+                if (dPad.up && !before.up) sendInput("dPadUp", true);
+                if (dPad.down && !before.down) sendInput("dPadDown", true);
+                if (dPad.left && !before.left) sendInput("dPadLeft", true);
+                if (dPad.right && !before.right) sendInput("dPadRight", true);
+                if (!dPad.up && before.up) sendInput("dPadUp", false);
+                if (!dPad.down && before.down) sendInput("dPadDown", false);
+                if (!dPad.left && before.left) sendInput("dPadLeft", false);
+                if (!dPad.right && before.right) sendInput("dPadRight", false);
+            }} on:touchmove={(e) => {
+                e.preventDefault();
+                let r = e.currentTarget.getBoundingClientRect().width / 2;
+                let cx = e.currentTarget.getBoundingClientRect().x + r;
+                let cy = e.currentTarget.getBoundingClientRect().y + r;
+                let touch = e.changedTouches[0];
+                let before = Object.assign({}, dPad);
+                if (touch.clientY < cy - r * 0.4) {
+                    dPad.up = true;
+                    dPad.down = false;
+                }
+                else if (touch.clientY > cy + r * 0.4) {
+                    dPad.down = true;
+                    dPad.up = false;
+                }
+                else {
+                    dPad.down = false;
+                    dPad.up = false;
+                }
+                if (touch.clientX < cx - r * 0.4) {
+                    dPad.left = true;
+                    dPad.right = false;
+                }
+                else if (touch.clientX > cx + r * 0.4) {
+                    dPad.right = true;
+                    dPad.left = false;
+                }
+                else {
+                    dPad.right = false;
+                    dPad.left = false;
+                }
+                if (dPad.up && !before.up) sendInput("dPadUp", true);
+                if (dPad.down && !before.down) sendInput("dPadDown", true);
+                if (dPad.left && !before.left) sendInput("dPadLeft", true);
+                if (dPad.right && !before.right) sendInput("dPadRight", true);
+                if (!dPad.up && before.up) sendInput("dPadUp", false);
+                if (!dPad.down && before.down) sendInput("dPadDown", false);
+                if (!dPad.left && before.left) sendInput("dPadLeft", false);
+                if (!dPad.right && before.right) sendInput("dPadRight", false);
+            }} on:touchend={() => {
+                let before = Object.assign({}, dPad);
+                dPad.up = false;
+                dPad.down = false;
+                dPad.left = false;
+                dPad.right = false;
+                if (!dPad.up && before.up) sendInput("dPadUp", false);
+                if (!dPad.down && before.down) sendInput("dPadDown", false);
+                if (!dPad.left && before.left) sendInput("dPadLeft", false);
+                if (!dPad.right && before.right) sendInput("dPadRight", false);
+            }} />
+        <polygon points="150,130 160,120 160,60 140,60 140,120" fill="grey" style:pointer-events="none" stroke="grey" stroke-width="17" stroke-linejoin="round"
+            opacity={dPad.up ? 0.5 : 1} />
+        <polygon points="130,150 120,160 60,160 60,140 120,140" fill="grey" style:pointer-events="none" stroke="grey" stroke-width="17" stroke-linejoin="round"
+            opacity={dPad.left ? 0.5 : 1}/>
+        <polygon points="150,170 160,180 160,240 140,240 140,180" fill="grey" style:pointer-events="none" stroke="grey" stroke-width="17" stroke-linejoin="round"
+            opacity={dPad.down ? 0.5 : 1} />
+        <polygon points="170,150 180,160 240,160 240,140 180,140" fill="grey" style:pointer-events="none" stroke="grey" stroke-width="17" stroke-linejoin="round"
+            opacity={dPad.right ? 0.5 : 1} />
     </g>
 </svg>
 
