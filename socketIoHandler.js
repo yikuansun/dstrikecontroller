70d7ab8b3b468f97d8cd9808ee3022a7af1f7195
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-28 19:47:07 -0400

    Enable CORS
    Making a public API (ur welcome)

--- socketIoHandler.js
+++ socketIoHandler.js
@@ -5,7 +5,12 @@ let sockets = {};
 
 /** @param {import('http').Server} server */
 export default function injectSocketIO(server) {
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST']
+        }
+    });
 
     io.on('connection', (socket) => {
         console.log(socket.id, "connected");
